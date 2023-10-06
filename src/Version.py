30eaff716ec3ca2c0106b9d1af344db4e210f9b7
root <swmaniacster@gmail.com>
2023-10-06 12:44:58 +0200

    1.2.11

--- src/Version.py
+++ src/Version.py
@@ -21,6 +21,6 @@
 
 PLUGIN = "Shadow-FHD"
 ID = "SHA"
-VERSION = "1.1.16"
+VERSION = "1.2.11"
 COPYRIGHT = "2018-2023 by dream-alpha"
 LICENSE = "This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version."
